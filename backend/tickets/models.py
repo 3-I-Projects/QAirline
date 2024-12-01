ccc512c2d415c26e4f6997722d162cedd4887c78
2uan2 <2uan20004@gmail.com>
2024-12-01 22:13:13 +0700

    did something

--- backend/tickets/models.py
+++ backend/tickets/models.py
@@ -8,6 +8,9 @@ class Ticket(models.Model):
     booked_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets')
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='tickets')
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='bookings')
-    seat = models.OneToOneField(Seat, on_delete=models.CASCADE, related_name='ticket') 
+    seat = models.OneToOneField(Seat, on_delete=models.CASCADE, related_name='ticket')
+    price = models.IntegerField()
+    ordered_time = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return f'Flight to {self.flight.destination_airport} for {self.customer}'
