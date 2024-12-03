54e65c0dc7fae011eccd848fb2acfaa6251177b0
GoatYogurt <84892539+GoatYogurt@users.noreply.github.com>
2024-12-03 08:48:14 +0700

    fix database bugs

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from users.models import CustomUser
 
 # Create your models here.
 class Post(models.Model):
@@ -19,7 +19,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     created_at = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, null=True)
 
     def type(self):
         if isinstance(self, Announcement):
