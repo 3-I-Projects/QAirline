8be48e58f77e582ebf1054c64eb91bfae05218e3
GoatYogurt <84892539+GoatYogurt@users.noreply.github.com>
2024-12-14 15:32:39 +0700

    added 'hello {user}' to header, fix dashboard css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+import { jwtDecode } from 'jwt-decode';
 
 const Header = () => {
+  const auth = useAuth();
+  const username =  (auth.accessToken) ? jwtDecode(auth.accessToken).username : 'guest';
   return (
     <div>
         <Link to='/'>Normal Homepage</Link>
         <span> | </span>
         <Link to='http://localhost:8000/admin'>Backend Admin</Link>
+
+        <p>Hello, {username}</p>
     </div>
   )
 }
--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -5,12 +5,10 @@ import { useAuth } from '../../context/AuthContext';
 const Dashboard = () => {
   const auth = useAuth();
   return (
-    <div className='container'>
       <div>
-        <h1>Welcome again, motherfucker! You're in admin dashboard.</h1>
+        <p>Welcome again, motherfucker! You're in admin dashboard.</p>
         <button onClick={() => auth.logoutAction()}>Logout</button>
       </div>
-    </div>
   )
 }
 
