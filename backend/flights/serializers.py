572aa1ee187ba490622e1ba2507be4f3c3ab2955
2uan2 <2uan20004@gmail.com>
2024-12-15 00:59:06 +0700

    added fields to flights serialzier

--- backend/flights/serializers.py
+++ backend/flights/serializers.py
@@ -12,9 +12,17 @@ class PlaneSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class FlightSerializer(serializers.ModelSerializer):
+    origin_airport_code = serializers.SerializerMethodField()
+    
     class Meta:
         model = Flight
-        fields = '__all__'
+        fields = ['id', 'plane', 'origin_airport', 'destination_airport', 'departure_time', 'delay', 'base_price', 'arrival_time', 'origin_airport_code']
+
+    def get_origin_airport_code(self, obj):
+        return obj.origin_airport.code  # Access the airport's code
+
+    def get_destination_airport_code(self, obj):
+        return obj.destination_airport.code
     
 class SeatSerializer(serializers.ModelSerializer):
     class Meta:
