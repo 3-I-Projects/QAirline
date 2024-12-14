eb1db80c317f2aa719b1865fb6fd07fed7e364c0
2uan2 <2uan20004@gmail.com>
2024-12-15 01:14:26 +0700

    added more things to flight serializer

--- backend/flights/serializers.py
+++ backend/flights/serializers.py
@@ -13,16 +13,25 @@ class PlaneSerializer(serializers.ModelSerializer):
 
 class FlightSerializer(serializers.ModelSerializer):
     origin_airport_code = serializers.SerializerMethodField()
+    origin_airport_city = serializers.SerializerMethodField()
+    destination_airport_code = serializers.SerializerMethodField()
+    destination_airport_city = serializers.SerializerMethodField()
     
     class Meta:
         model = Flight
-        fields = ['id', 'plane', 'origin_airport', 'destination_airport', 'departure_time', 'delay', 'base_price', 'arrival_time', 'origin_airport_code']
+        fields = ['id', 'plane', 'origin_airport', 'destination_airport', 'departure_time', 'delay', 'base_price', 'arrival_time', 'origin_airport_code', 'origin_airport_city', 'destination_airport_code', 'destination_airport_city']
 
     def get_origin_airport_code(self, obj):
         return obj.origin_airport.code  # Access the airport's code
 
     def get_destination_airport_code(self, obj):
         return obj.destination_airport.code
+    
+    def get_origin_airport_city(self, obj):
+        return obj.origin_airport.city
+    
+    def get_destination_airport_city(self, obj):
+        return obj.destination_airport.city
     
 class SeatSerializer(serializers.ModelSerializer):
     class Meta:
