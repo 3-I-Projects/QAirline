fa387a693b5b239e08c2aff77bfa02e23d7f5d08
2uan2 <2uan20004@gmail.com>
2024-12-15 23:49:48 +0700

    improve some pages

--- frontend/src/context/BookingContext.jsx
+++ frontend/src/context/BookingContext.jsx
@@ -1,24 +1,41 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const BookingContext = createContext(null);
 export { BookingContext };
 export default function BookingContextProvider({ children }) {
-  const [count, setCount] = useState(0);
-  const [customerCount, setCustomerCount] = useState(2);
-  const [allCustomers, setAllCustomers] = useState(
-    Array(customerCount).fill({
-      id: "",
-      first_name: "",
-      last_name: "",
-      birthday: "",
-      country: "Viet Nam",
-      phone_number: "",
-      email: "",
-      gender: "",
-    })
-  );
+  const [ count, setCount ] = useState(0);
+  const [ customerCount, setCustomerCount ] = useState(3);
+  const [ allCustomers, setAllCustomers ] = useState([]);
+  const [ tickets, setTickets ] = useState([]);
+  useEffect(() => {
+      setAllCustomers(
+        Array(customerCount).fill({
+          id: "",
+          first_name: "",
+          last_name: "",
+          birthday: "",
+          country: "Viet Nam",
+          phone_number: "",
+          email: "",
+          gender: "",
+        })
+      );
+      setTickets(
+        Array(customerCount).fill({
+          id: "",
+          price: "",
+          ordered_time: "",
+          status: "",
+          booked_by: "Viet Nam",
+          customer: "",
+          flight: "",
+          seat: "",
+        })
+      );
+}, [customerCount]);
+
   return (
-    <BookingContext.Provider value={{ count, setCount, allCustomers, setAllCustomers, customerCount, setCustomerCount }}>
+    <BookingContext.Provider value={{ count, setCount, allCustomers, setAllCustomers, customerCount, setCustomerCount, tickets, setTickets }}>
       {children}
     </BookingContext.Provider>
   );
