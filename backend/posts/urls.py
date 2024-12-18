4d399895d9325ff2daa82f3d68fce6522531210e
GoatYogurt <84892539+GoatYogurt@users.noreply.github.com>
2024-12-18 21:16:43 +0700

    apply router for posts

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -8,25 +8,13 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'announcementCategorys', AnnouncementCategoryViewSet, basename='announcementCategory')
 router.register(r'announcements', AnnouncementViewSet, basename='announcement')
-# router.register('discountCategory', DiscountCategoryViewSet)
-# router.register('discount', DiscountViewSet)
-# router.register('informationCategory', InformationCategoryViewSet)
-# router.register('information', InformationViewSet)
-# router.register('newsCategory', NewsCategoryViewSet)
-# router.register('news', NewsViewSet)
+router.register(r'discountCategory', DiscountCategoryViewSet, basename='discountCategory')
+router.register(r'discount', DiscountViewSet, basename='discount')
+router.register(r'informationCategory', InformationCategoryViewSet, basename='informationCategory')
+router.register(r'information', InformationViewSet, basename='information')
+router.register(r'newsCategory', NewsCategoryViewSet, basename='newsCategory')
+router.register(r'news', NewsViewSet, basename='news')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('discounts/', DiscountList.as_view(), name='discount-list'),
-    path('discounts/<int:pk>', DiscountDetail.as_view(), name='discount-detail'),
-    path('discountCategories/', DiscountCategoryList.as_view(), name='discountCategory-list'),
-    path('discountCategories/<int:pk>', DiscountCategoryDetail.as_view(), name='discountCategory-detail'),
-    path('informations/', InformationList.as_view(), name='information-list'),
-    path('informations/<int:pk>', InformationDetail.as_view(), name='information-detail'),
-    path('informationCategories/', InformationCategoryList.as_view(), name='informationCategory-list'),
-    path('informationCategories/<int:pk>', InformationCategoryDetail.as_view(), name='informationCategory-detail'),
-    path('news/', NewsList.as_view(), name='news-list'),
-    path('news/<int:pk>', NewsDetail.as_view(), name='news-detail'),
-    path('newsCategories/', NewsCategoryList.as_view(), name='newsCategory-list'),
-    path('newsCategories/<int:pk>', NewsCategoryDetail.as_view(), name='newsCategory-detail'),
 ]
